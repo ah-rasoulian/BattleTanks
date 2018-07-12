0ef45629045e0bf3d8214c931ce2809734e47d58
hasan rashidi <mhrashidi77@gmail.com>
2018-07-12 19:15:44 +0430

    print public ip

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Server implements Runnable
 {
@@ -14,11 +13,24 @@ public class Server implements Runnable
         serverConnected = false;
     }
     public String showIp (){
-        try {
-            return InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+        String systemIpAddress = "";
+        try
+        {
+            URL url_name = new URL("http://bot.whatismyipaddress.com");
+
+            BufferedReader sc =
+                    new BufferedReader(new InputStreamReader(url_name.openStream()));
+
+            // reads system IPAddress
+            systemIpAddress = sc.readLine().trim();
+            return systemIpAddress;
         }
+        catch (Exception e)
+        {
+            systemIpAddress = "Cannot Execute Properly";
+        }
+
+//            return InetAddress.getLocalHost().getHostAddress();
         return "";
     }
 
